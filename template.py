490be85a822dd661684b7f9670f0c367f43dd64a
patilgaurav1499 <patilgaurav77606@gmail.com>
2024-06-08 19:00:28 +0530

    Ok

--- template.py
+++ template.py
@@ -7,7 +7,7 @@ from pathlib import Path #to create system compatible path ['/']
 lis_of_files = [
     ".github/workflows/.gitkeep", #Github folder will be used for deployemnt, entire configuration for continuos integration and Continuous deployement
     "src/__init__.py", #src folder contains entire source code
-    "src/components/__init__.py", #components folder has to be used for different ML components like data ingestion, data transformation, fe, model training, model eval 
+    "src/components/__init__.py", #components folder has to be used for different ML components like data ingestion, data transformation, fe, model training, model evaluation
     "src/components/data_ingestion.py", 
     "src/components/data_transformation.py",
     "src/components/model_trainer.py",
@@ -15,19 +15,19 @@ lis_of_files = [
     "src/pipeline/__init__.py",
     "src/pipeline/training_pipeline.py",
     "src/pipeline/prediction_pipeline.py",
-    "src/utils/__init__.py",
-    "src/utils/utils.py",
+    "src/utils/__init__.py", #for writing utility methods
+    "src/utils/utils.py", #for writing utility methods
     "src/logger/logging.py",
     "src/exception/exception.py",
-    "tests/unit/__init__.py",
-    "tests/integration/__init__.py",
-    "init_setup.sh",
+    "tests/unit/__init__.py", #file for unit testing 
+    "tests/integration/__init__.py", #file for integration testing
+    "init_setup.sh", #to initialize the setup
     "requirements.txt",
     "requirements_dev.txt",
     "setup.py",
     "setup.cfg",
-    "pyproject.toml",
-    "tox.ini",
+    "pyproject.toml", 
+    "tox.ini", #Used for testing project in local developement environment
     "experiment/experiments.ipynb" #Exp folder has to be used for experiement like diff small codes instead of using python kernel
 ]
 
